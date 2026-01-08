5f3f3a4470fbcd1d0e77941ca30e05c13c623b21
ysow <ysow@scaleway.com>
2026-01-08 10:43:54 +0000

    update controller to new karpenter api

--- controller.go
+++ controller.go
@@ -7,9 +7,12 @@ import (
 
 	"github.com/scaleway/scaleway-sdk-go/api/instance/v1"
 	"github.com/scaleway/scaleway-sdk-go/scw"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// Corrected import path for the Karpenter v1beta1 API
+	karpenterv1beta1 "sigs.k8s.io/karpenter/pkg/apis/v1beta1"
+
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -18,46 +21,6 @@ import (
 
 const ScalewayFinalizer = "scaleway.com/finalizer"
 
-// NodeClaim is a local stub that mimics the Karpenter NodeClaim.
-// This is used to avoid a direct dependency on the Karpenter API machinery.
-// +kubebuilder:object:root=true
-type NodeClaim struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec NodeClaimSpec `json:"spec,omitempty"`
-}
-
-// NodeClaimSpec defines the desired state of NodeClaim
-type NodeClaimSpec struct {
-	// This is a simplified spec for the stub.
-	ProvisionerName string `json:"provisionerName,omitempty"`
-}
-
-// Required methods to implement runtime.Object
-func (in *NodeClaim) DeepCopyObject() runtime.Object {
-	if c := in.DeepCopy(); c != nil {
-		return c
-	}
-	return nil
-}
-
-func (in *NodeClaim) DeepCopy() *NodeClaim {
-	if in == nil {
-		return nil
-	}
-	out := new(NodeClaim)
-	in.DeepCopyInto(out)
-	return out
-}
-
-func (in *NodeClaim) DeepCopyInto(out *NodeClaim) {
-	*out = *in
-	out.TypeMeta = in.TypeMeta
-	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
-	out.Spec = in.Spec
-}
-
 // ScalewayReconciler reconciles a NodeClaim object.
 type ScalewayReconciler struct {
 	client.Client
@@ -65,17 +28,38 @@ type ScalewayReconciler struct {
 	scwClient *scw.Client
 }
 
+//+kubebuilder:rbac:groups=karpenter.sh,resources=nodeclaims,verbs=get;list;watch;update;patch
+
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
 func (r *ScalewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
-	var nodeClaim NodeClaim
+	var nodeClaim karpenterv1beta1.NodeClaim
 	if err := r.Get(ctx, req.NamespacedName, &nodeClaim); err != nil {
-		return ctrl.Result{}, client.IgnoreNotFound(err)
+		if errors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
 	}
 
-	if nodeClaim.Labels["karpenter.sh/capacity-type"] != "scaleway-gpu" {
+	// Check for the specific capacity type
+	isScalewayGPU := false
+	for _, requirement := range nodeClaim.Spec.Requirements {
+		if requirement.Key == karpenterv1beta1.CapacityTypeLabel && requirement.Operator == v1.NodeSelectorOpIn {
+			for _, value := range requirement.Values {
+				if value == "scaleway-gpu" {
+					isScalewayGPU = true
+					break
+				}
+			}
+		}
+		if isScalewayGPU {
+			break
+		}
+	}
+
+	if !isScalewayGPU {
 		logger.Info("ignoring nodeclaim, not a scaleway-gpu capacity type", "nodeclaim", req.NamespacedName)
 		return ctrl.Result{}, nil
 	}
@@ -90,7 +74,8 @@ func (r *ScalewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 		}
 	} else {
 		if controllerutil.ContainsFinalizer(&nodeClaim, ScalewayFinalizer) {
-			logger.Info("TODO: Supprimer l'instance Scaleway")
+			logger.Info("TODO: Delete the Scaleway instance")
+			// Add deletion logic here
 
 			controllerutil.RemoveFinalizer(&nodeClaim, ScalewayFinalizer)
 			if err := r.Update(ctx, &nodeClaim); err != nil {
@@ -110,20 +95,27 @@ func (r *ScalewayReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 	return ctrl.Result{}, nil
 }
 
-func (r *ScalewayReconciler) provisionInstance(ctx context.Context, nodeClaim *NodeClaim) (*instance.Server, error) {
+func (r *ScalewayReconciler) provisionInstance(ctx context.Context, nodeClaim *karpenterv1beta1.NodeClaim) (*instance.Server, error) {
 	logger := log.FromContext(ctx)
 
-	// Get instance type from NodeClaim labels
-	instanceTypeLabel := "karpenter.sh/instance-type"
-	instanceType, ok := nodeClaim.Labels[instanceTypeLabel]
-	if !ok {
-		return nil, fmt.Errorf("nodeclaim %q does not have label %q", nodeClaim.Name, instanceTypeLabel)
+	// Get instance type from NodeClaim requirements
+	var instanceType string
+	for _, requirement := range nodeClaim.Spec.Requirements {
+		if requirement.Key == v1.LabelInstanceTypeStable && requirement.Operator == v1.NodeSelectorOpIn {
+			if len(requirement.Values) > 0 {
+				instanceType = requirement.Values[0] // Taking the first value
+				break
+			}
+		}
+	}
+
+	if instanceType == "" {
+		return nil, fmt.Errorf("nodeclaim %q does not have required label %q", nodeClaim.Name, v1.LabelInstanceTypeStable)
 	}
 
 	// Translate to Scaleway commercial type
 	commercialType, err := getCommercialType(instanceType)
 	if err != nil {
-		// Log and return error if instance type is not supported
 		logger.Error(err, "unsupported instance type", "instance-type", instanceType)
 		return nil, err
 	}
@@ -138,7 +130,7 @@ func (r *ScalewayReconciler) provisionInstance(ctx context.Context, nodeClaim *N
 
 	createServerReq := &instance.CreateServerRequest{
 		Zone:           "fr-par-1",
-		CommercialType: commercialType, // Use dynamic commercial type
+		CommercialType: commercialType,
 		Image:          "ubuntu_jammy_gpu_os_12",
 		Name:           nodeClaim.Name,
 		CloudInit:      &userData,
@@ -155,8 +147,6 @@ func (r *ScalewayReconciler) provisionInstance(ctx context.Context, nodeClaim *N
 
 // getCommercialType translates a Karpenter instance type to a Scaleway commercial type.
 func getCommercialType(instanceType string) (string, error) {
-	// This map translates Karpenter instance type names to Scaleway commercial types.
-	// This should be expanded based on the types you want to support.
 	instanceTypeMap := map[string]string{
 		"l4":   "L4-1-24G",
 		"l40s": "L40S-1-48G",
@@ -171,57 +161,12 @@ func getCommercialType(instanceType string) (string, error) {
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *ScalewayReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	// Because we are not using the real Karpenter API, we need to manually
-	// add our stub NodeClaim type to the manager's scheme so that the
-	// controller can watch for it.
-	// We use the same GroupVersion as the real Karpenter API.
-	gv := schema.GroupVersion{Group: "karpenter.sh", Version: "v1alpha5"}
-	if err := mgr.GetScheme().AddKnownTypes(
-		gv,
-		&NodeClaim{},
-		&NodeClaimList{},
-	); err != nil {
+    // Add karpenter v1beta1 to the scheme
+	if err := karpenterv1beta1.SchemeBuilder.AddToScheme(mgr.GetScheme()); err != nil {
 		return err
 	}
-	metav1.AddToGroupVersion(mgr.GetScheme(), gv)
 
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&NodeClaim{}).
+		For(&karpenterv1beta1.NodeClaim{}).
 		Complete(r)
-}
-
-// +kubebuilder:object:root=true
-type NodeClaimList struct {
-	metav1.TypeMeta `json:",inline"`
-	metav1.ListMeta `json:"metadata,omitempty"`
-	Items           []NodeClaim `json:"items"`
-}
-
-func (in *NodeClaimList) DeepCopyObject() runtime.Object {
-	if c := in.DeepCopy(); c != nil {
-		return c
-	}
-	return nil
-}
-
-func (in *NodeClaimList) DeepCopy() *NodeClaimList {
-	if in == nil {
-		return nil
-	}
-	out := new(NodeClaimList)
-	in.DeepCopyInto(out)
-	return out
-}
-
-func (in *NodeClaimList) DeepCopyInto(out *NodeClaimList) {
-	*out = *in
-	out.TypeMeta = in.TypeMeta
-	in.ListMeta.DeepCopyInto(&out.ListMeta)
-	if in.Items != nil {
-		in, out := &in.Items, &out.Items
-		*out = make([]NodeClaim, len(*in))
-		for i := range *in {
-			(*in)[i].DeepCopyInto(&(*out)[i])
-		}
-	}
 }
